c4d02f7b8d0ea84b283aa2e3361abf494d8477a4
mco <cordebard@gmail.com>
2016-04-17 19:32:09 +0200

    Fix coverage pb

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,69 @@
+// Build stuff
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Test stuff
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
+
+// Debug stuff
+var filelog = require('gulp-filelog');
+
+// Load config
 var tsProject = ts.createProject('./tsconfig.json');
 
 
-gulp.task('build', function() {
-     return gulp.src('./app/**/*.ts', { base: '.' })
-     .pipe(ts(tsProject))
-     .pipe(gulp.dest('./dist'));
+gulp.task('build', function () {
+    return gulp.src('./app/**/*.ts', { base: '.' })
+        .pipe(filelog())
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject))
+        .pipe(sourcemaps.write()) // Now the sourcemaps are added to the .js file
+        .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('test', function() {
+gulp.task('pre-test', function () {
+    return gulp.src(['./dist/app/**/*.js'])
+        //.pipe(filelog())
+        // Covering files
+        .pipe(istanbul())
+        // Force `require` to return covered files
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function () {
     //find test code - note use of 'base'
     return gulp.src('./test/**/*.ts', { base: '.' })
-    /*transpile*/
-    .pipe(ts(tsProject))
-    /*flush to disk*/
-    .pipe(gulp.dest('./dist'))
-    /*execute tests*/
-    .pipe(mocha({
-        reporter: 'progress'
-    }));
+        // Transpile
+        .pipe(ts(tsProject))
+        // Flush to disk
+        .pipe(gulp.dest('./dist'))
+        // Execute tests
+        .pipe(mocha({ reporter: 'progress' }))
+        // Create the reports after tests ran
+        .pipe(istanbul.writeReports())
+    // Enforce a coverage of at least 90%
+    //.pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));;
 });
 
-gulp.task('watch', function() {
+gulp.task('remap-istanbul', function () {
+    return gulp.src('./coverage/coverage-final.json', { base: '.' })
+        .pipe(filelog())
+        .pipe(remapIstanbul({
+            reports: {
+                'lcovonly': './coverage/remap/lcov.info',
+                'json': './coverage/remap/coverage.json',
+                'html': './coverage/remap/html-report',
+                'text-summary': './coverage/remap/text-summary.txt'
+            }
+        }));
+});
+
+gulp.task('watch', function () {
     gulp.watch(['app/**/*.ts'], ['build']);
-    gulp.watch(['test/**/*.ts'], ['test']);
+    gulp.watch(['test/**/*.ts'], ['test', 'remap-istanbul']);
 });
 
 
--- test/service/authServiceTests.ts
+++ test/service/authServiceTests.ts
@@ -1,16 +1,19 @@
 /// <reference path="../testRefPath.ts"/>
 
+import "reflect-metadata";
 import { Kernel } from "inversify";
 import { AuthService, AuthServiceImpl } from "../../app/services/authService";
 import { HttpService } from "../../app/services/httpService";
+import { injectable, inject } from "inversify";
 
+@injectable()
 class HttpServiceMock implements HttpService {
     public startRestServer() : Promise<any> {
         return null;
     }
     
     public addGetRoute() {
-        console.log("cocuocu");
+        console.log("cocuo s"); 
     }
 }
 
@@ -21,9 +24,8 @@ describe('AuthenticationService', function() {
         let kernel = new Kernel();
         kernel.bind<AuthService>("AuthService").to(AuthServiceImpl).inSingletonScope();
         kernel.bind<HttpService>("HttpService").to(HttpServiceMock).inSingletonScope();
-        
         var authService = kernel.get<AuthService>("AuthService");
-        
+               
     });
   });
 }); 
